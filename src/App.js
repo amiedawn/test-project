e3a5eefd8ce46ea04c59622570720f145b3a2a6d
amiedawn <amie.d.carroll@gmail.com>
2022-01-01 19:38:21 -0700

    reminder class and delete work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import {useState} from 'react';
 import Header from './components/Header';
 import './App.css';
+import Tasks from './components/Tasks';
 
 function App() {
- 
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30 pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: false,
+    }
+])
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  };
 
   return (
     <div className="container">
       <Header />
-      
+      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} setTasks={setTasks} /> : "No tasks are available."}
     </div>
   );
 }
